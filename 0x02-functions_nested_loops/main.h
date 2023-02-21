d31110567ed631bc0f56e577a009407add7a318a
Alimazoya David <davidalimazo@gmail.com>
2023-02-21 14:09:47 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,28 +1,6 @@
-/*
- * File: 0-holberton.c
- * Auth: Brennan D Baraban
- *
- */
+#ifndef MAIN_H
+#define MAIN_H
 
-#include "holberton.h"
+int _putchar(char c);
 
-/**
- * main - Prints "Holberton" followed by a new line.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
-
-	return (0);
-}
+#endif
